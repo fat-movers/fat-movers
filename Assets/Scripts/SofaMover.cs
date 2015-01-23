71086b23ebaa671e110d5b04c5e1affa34721b4d
Jarkka <jarkka.salminen@createtrips.com>
2015-01-23 23:27:39 +0200

    Added: Moving with force to one direction works

--- Assets/Scripts/SofaMover.cs
+++ Assets/Scripts/SofaMover.cs
@@ -11,7 +11,9 @@ public class SofaMover : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)){
-			rigidbody.AddRelativeForce(Vector3.left *300);
+			rigidbody.AddRelativeForce(Vector3.left *1000);
+			Vector3 currentDir = transform.TransformDirection(Vector3.left);
+			Debug.DrawRay(transform.position, currentDir, Color.white, 1.0f);
 		}else if(Input.GetKey(KeyCode.A)){
 			
 		}else if(Input.GetKey(KeyCode.D)){
