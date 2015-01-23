0896a16ddeeb675b6a47d383666439369bb5e8e7
Jarkka <jarkka.salminen@createtrips.com>
2015-01-23 23:09:38 +0200

    Added: Initial stuff

---
+++ Assets/Scripts/SofaMover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SofaMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)){
+			rigidbody.AddRelativeForce(Vector3.left *300);
+		}else if(Input.GetKey(KeyCode.A)){
+			
+		}else if(Input.GetKey(KeyCode.D)){
+			
+		}
+	}
+}
