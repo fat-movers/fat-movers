3d1d17e10ce6d210d0bf905f46ee420a21c56a87
jehna <jesse.luoto@gmail.com>
2015-01-24 12:34:50 +0200

    Added: Sofa Target

---
+++ Assets/Scripts/Triggers/SofaTarget.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SofaTarget : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other) {
+		if(collider.bounds.Contains(other.bounds.min) && collider.bounds.Contains(other.bounds.max) && other.gameObject.GetComponent<Sofa>() != null) {
+			Debug.Log("Sohva jeejee");
+		}
+	}
+}
