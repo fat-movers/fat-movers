8491a6cdfb165a69b1f19b3ebb5d13dbc5f30dee
jehna <jesse.luoto@gmail.com>
2015-01-24 12:44:13 +0200

    Added: Sofa target rotation detection

--- Assets/Scripts/Triggers/SofaTarget.cs
+++ Assets/Scripts/Triggers/SofaTarget.cs
@@ -14,8 +14,14 @@ public class SofaTarget : MonoBehaviour {
 	}
 
 	void OnTriggerStay(Collider other) {
-		if(collider.bounds.Contains(other.bounds.min) && collider.bounds.Contains(other.bounds.max) && other.gameObject.GetComponent<Sofa>() != null) {
+		if(collider.bounds.Contains(other.bounds.min) &&
+		   collider.bounds.Contains(other.bounds.max) &&
+		   other.gameObject.GetComponent<Sofa>() != null &&
+		   Mathf.Abs(other.gameObject.transform.rotation.y - transform.rotation.y) < 0.10f) {
+			Debug.Log(other.gameObject.transform.rotation.y + " " + transform.rotation.y);
 			Debug.Log("Sohva jeejee");
 		}
+
+
 	}
 }
