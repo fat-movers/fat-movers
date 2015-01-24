76bf602da5f2d16e8023f3fb3b4f3ecb52ffeb87
jehna <jesse.luoto@gmail.com>
2015-01-24 13:51:20 +0200

    The game is playable

---
+++ Assets/Scripts/Dummy Scripts/Restarter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Restarter : MonoBehaviour {
+	
+	public void ReStart () {
+		Application.LoadLevel(Application.loadedLevel);
+		Time.timeScale = 1;
+	}
+}
--- Assets/Scripts/Triggers/SofaTarget.cs
+++ Assets/Scripts/Triggers/SofaTarget.cs
@@ -3,25 +3,27 @@ using System.Collections;
 
 public class SofaTarget : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	public GameObject gameEnderObject;
+	private bool won = false;
 
 	void OnTriggerStay(Collider other) {
 		if(collider.bounds.Contains(other.bounds.min) &&
 		   collider.bounds.Contains(other.bounds.max) &&
 		   other.gameObject.GetComponent<Sofa>() != null &&
 		   Mathf.Abs(other.gameObject.transform.rotation.y - transform.rotation.y) < 0.10f) {
-			Debug.Log(other.gameObject.transform.rotation.y + " " + transform.rotation.y);
-			Debug.Log("Sohva jeejee");
+			//Debug.Log(other.gameObject.transform.rotation.y + " " + transform.rotation.y);
+			//Debug.Log("Sohva jeejee");
+			Win();
 		}
 
 
+	}
+
+	void Win() {
+		if (won) return;
+		won = true;
+		Time.timeScale = 0;
+		Instantiate (gameEnderObject);
+		Debug.Log ("You win!");
 	}
 }
