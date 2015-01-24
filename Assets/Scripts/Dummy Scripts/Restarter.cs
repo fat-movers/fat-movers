390e2ad6bf542bdc21497810a2a53eb2efae6f6a
jehna <jesse.luoto@gmail.com>
2015-01-24 14:35:16 +0200

    Added: Game and menu playable

--- Assets/Scripts/Dummy Scripts/Restarter.cs
+++ Assets/Scripts/Dummy Scripts/Restarter.cs
@@ -2,9 +2,8 @@
 using System.Collections;
 
 public class Restarter : MonoBehaviour {
-	
+
 	public void ReStart () {
-		Application.LoadLevel(Application.loadedLevel);
-		Time.timeScale = 1;
+		FindObjectOfType<SuperManager> ().gameManager.MoveToScene ("mainmenu");
 	}
 }
