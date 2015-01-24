d06c994308000eb05a413eb880c04a25001abe4b
Jarkka <jarkka.salminen@createtrips.com>
2015-01-24 16:04:02 +0200

    Added: TheMaster that creates SuperManager singleton

--- Assets/Scripts/SuperManager.cs
+++ Assets/Scripts/SuperManager.cs
@@ -7,6 +7,19 @@ public class SuperManager : MonoBehaviour {
 	public MenuManager menuManager;
 	public SoundManager soundManager;
 
+	private static SuperManager superManagerInstance;
+
+	public static SuperManager instance{
+		get{ 
+			if(!superManagerInstance){
+				GameObject go = Instantiate(Resources.Load("SuperManager")) as GameObject;
+				superManagerInstance = go.GetComponent<SuperManager>();
+			}
+
+			return superManagerInstance;
+		}
+	}
+
 	void Awake() {
 		DontDestroyOnLoad(gameObject);
 	}
